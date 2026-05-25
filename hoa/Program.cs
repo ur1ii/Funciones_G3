37027f2afeb4f2300bf3c8edc3b25931b0628338
ISDA <isabellada@uamv.edu.ni>
2026-05-25 11:02:33 -0600

    feat: validar_edad

--- hoa/Program.cs
+++ hoa/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+bool EsMayorDeEdad(int edad)
+{
+    return edad >= 18;
+}
+
+Console.Write("Ingrese su edad: ");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+if (EsMayorDeEdad(edad))
+{
+    Console.WriteLine("Usted es mayor de edad.");
+}
+else
+{
+    Console.WriteLine("Usted es menor de edad.");
+}
---
+++ hoa/ejercicio7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+double CalcularPotencia(double baseNum, double exponente)
+{
+    return Math.Pow(baseNum, exponente);
+}
+
+Console.Write("Ingrese la base: ");
+double baseNum = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese el exponente: ");
+double exponente = Convert.ToDouble(Console.ReadLine());
+
+double resultado = CalcularPotencia(baseNum, exponente);
+
+Console.WriteLine("El resultado es: " + resultado);
