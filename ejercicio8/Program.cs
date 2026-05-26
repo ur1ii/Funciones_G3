f365bf6db40f8b6606923a3d533fd1028ec2485c
Isabella <isabellada@uamv.edu.ni>
2026-05-26 09:33:53 -0600

    feat: actualizar ejercicio

--- ejercicio8/Program.cs
+++ ejercicio8/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Escribe un texto: ");
+string texto = Console.ReadLine();
+
+Console.WriteLine(texto.ToUpper());
+Console.WriteLine(texto.ToLower());
