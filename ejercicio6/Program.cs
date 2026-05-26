aedbe0cb71afe469903f443eb6dcc61d738ae7ae
Isabella <isabellada@uamv.edu.ni>
2026-05-26 12:08:29 -0600

    Feat: validar edades

---
+++ ejercicio6/Program.cs
@@ -0,0 +1,16 @@
+﻿bool EsMayorDeEdad(int edad)
+{
+    return edad >= 18;
+}
+
+Console.Write("Ingrese su edad: ");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+if (EsMayorDeEdad(edad))
+{
+    Console.WriteLine("Usted es mayor de edad.");
+}
+else
+{
+    Console.WriteLine("Usted es menor de edad.");
+}
