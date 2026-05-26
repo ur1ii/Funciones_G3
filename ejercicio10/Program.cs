2f9ee56f59b3ebf4d6cb5527b2b62a752a006a8f
Isabella <isabellada@uamv.edu.ni>
2026-05-26 10:25:11 -0600

    feat: Validar entradas numericas

---
+++ ejercicio10/Program.cs
@@ -0,0 +1,20 @@
+﻿double numero;
+bool valido = false;
+
+do
+{
+    Console.Write("Ingrese un número: ");
+    string entrada = Console.ReadLine() ?? "";
+
+    if (double.TryParse(entrada, out numero))
+    {
+        valido = true;
+    }
+    else
+    {
+        Console.WriteLine("Entrada inválida, intente de nuevo.");
+    }
+
+} while (!valido);
+
+Console.WriteLine("Número válido: " + numero);
